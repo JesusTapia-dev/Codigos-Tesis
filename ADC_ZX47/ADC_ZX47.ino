63b0bf972d792fd411b98d04808761e1bb703d0c
JesusTapia-dev <jtapiag@uni.pe>
2023-11-13 14:45:07 -0500

    Valores Analógicos raw
    Todo se ha modificado para que muestre los valores luego de un multi sampleo

--- ADC_ZX47/ADC_ZX47.ino
+++ ADC_ZX47/ADC_ZX47.ino
@@ -3,7 +3,7 @@
 int analogValue=0;
 float Vref=3100;// Voltaje de referencia para el ADC
 float Vout=0;
-float Plinea=57.6,PdBm=0;;// Valor entre 1 y 500 kW, las unidades son kW
+float Plinea=57.6,Vpk=0;;// Valor entre 1 y 500 kW, las unidades son kW
 int VminPot=1;//con esto quiere decir que potencia es la máxima
 bool serialData=0;//Un valor de 1 indica que se debe ingresar por monitor
 float VoutRef=1;
@@ -19,10 +19,11 @@ void setup() {
   String potLinChar = Serial.readStringUntil('\n');
   Plinea=atof(potLinChar.c_str());
   }
-  PdBm=10 *(log10(Plinea)-2);
-  VoutRef=ecuacionLineal(PdBm);
+  Vpk=sqrt(10*Plinea)/5;
+  VoutRef=ecuacionLineal(Vpk);
   Serial.print("Voltaje de salida de referencia: ");
-  Serial.println(VoutRef);
+  Serial.print(VoutRef);
+  Serial.print("Voltaje pkpk:"); Serial.print(" "); Serial.println(Vpk);
 
 }
 
@@ -35,7 +36,7 @@ void loop() {
   }
   int averageValue = total / numSamples;
   Vout = 0.8291*averageValue+90.27;//Ajuste realizado para el ADC
-  Serial.print(Vout);
+  Serial.println(Vout);
 }
 float ecuacionLineal(float PdBm){
   float m=-0.02451,b=1.048;
